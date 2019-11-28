dcb8d19990819a0a5e9fe9be2ba24b21b5f753c5
Thomas Lu <thomas-lu@live.com>
2019-11-28 03:27:42 -0500

    moving to python file for gpu server use

---
+++ models/ball_heatmap_ground_truth.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 17 15:21:37 2019
+
+@author: nrchilku
+"""
+import pygame
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main(width, height, samples, frames, save=False):
+    
+    X = []
+    Y = []
+    screen = pygame.display.set_mode((width, height))
+    end = False       
+    for sample_count in range(samples):
+        radius = np.random.randint(2, 7)
+        x = np.random.randint(radius + 5, width - radius - 5) # initial x-position
+        y = np.random.randint(radius + 5, height - radius - 5) # initial y-position
+        v_x = np.random.randint(-1, 1) # initial x-velocity
+        v_y = np.random.randint(-1, 1) # initial y-velocity
+        color = np.random.randint(50, 255, 3) # background is black so starting at 50
+        X_frames = []
+                            
+        for frame_count in range(frames):                
+            x += v_x
+            y += v_y
+            screen.fill((0, 0, 0))
+            pygame.draw.circle(screen, color, (x,y), radius)
+            X_frames.append(pygame.surfarray.array3d(screen))            
+            # check for bounce
+            if x <= radius or x >= width - radius:
+                v_x *= -1
+            if y <= radius or y >= height - radius:
+                v_y *= -1  
+            #clock.tick(30)
+            pygame.display.flip()
+        # append X and Y
+        X.append(np.array(X_frames))
+        # Y
+        grayscale = np.dot(np.array(X_frames), np.array([0.2125, 0.7154, 0.0721]))
+        Y.append(grayscale.astype('uint8'))
+                
+    
+    if save:
+        np.save("X_width_" + str(width) + "_" + str(frames) + "_" + str(samples), np.array(X))
+        np.save("Y_width_" + str(width) + "_" + str(frames) + "_" + str(samples), np.array(Y))
+            
+    #return np.array(X), np.array(Y)
+    pygame.quit()
+                    
+                
+                
+                
+            
+
+        
---
+++ models/model.py
@@ -0,0 +1,89 @@
+import os
+import sys
+lmu_path = os.path.abspath("../lmu")
+sys.path.append(lmu_path)
+
+from lmu import LMUCell
+
+import keras as K
+from keras.applications import ResNet50
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import Dense, Input, GlobalAveragePooling2D, Conv2DTranspose, Reshape, Lambda, Conv2D, UpSampling2D
+from keras.layers import TimeDistributed
+from keras.layers.recurrent import RNN
+from keras.models import Sequential, Model
+from keras.initializers import Constant
+from keras.utils import multi_gpu_model, to_categorical
+
+import numpy as np
+
+def lmu_layer(return_sequences=False,**kwargs):
+    return RNN(LMUCell(units=6,
+                       order=6,
+                       theta=15,
+                       input_encoders_initializer=Constant(1),
+                       hidden_encoders_initializer=Constant(0),
+                       memory_encoders_initializer=Constant(0),
+                       input_kernel_initializer=Constant(0),
+                       hidden_kernel_initializer=Constant(0),
+                       memory_kernel_initializer='glorot_normal',
+                      ),
+               return_sequences=return_sequences,
+               **kwargs)
+
+def Lmu_stack(input_tensor, return_sequences):
+    t1 = []
+    t2 = []
+    shape = K.backend.int_shape(input_tensor)
+    input_tensor = Reshape([shape[1], -1, shape[-1]])(input_tensor)
+    for i in range(shape[-1]):
+        x = Lambda(lambda x: x[...,i])(input_tensor)
+        x = lmu_layer(return_sequences=return_sequences)(x)
+        t1.append(Dense(1)(x))
+        t2.append(Dense(49)(x))
+    return [Lambda(lambda x: K.backend.stack(x, axis=-1))(t1), Lambda(lambda x: K.backend.stack(x, axis=-1))(t2)]
+
+def velocity_layer(t):
+    t = TimeDistributed(Reshape([-1]))(t)
+    return TimeDistributed(Dense(2))(t)
+
+def deconvolution_layer(t):
+    heat = TimeDistributed(Reshape([7,7,-1]))(t)
+    heat = TimeDistributed(Conv2DTranspose(filters=32,kernel_size=3,strides=2, dilation_rate=1,activation='relu', padding='same'))(heat)
+    heat = TimeDistributed(UpSampling2D( interpolation='bilinear'))(heat)
+    heat = TimeDistributed(Conv2DTranspose(filters=8,kernel_size=3,strides=2, dilation_rate=1,activation='relu', padding='same'))(heat)
+    heat = TimeDistributed(UpSampling2D( interpolation='bilinear'))(heat)
+    heat = TimeDistributed(Conv2DTranspose(filters=1,kernel_size=3,strides=2, dilation_rate=1,activation='relu', padding='same'))(heat)
+    return heat
+
+seq_len = 15
+
+resnet_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224,224,3))
+
+for layer in resnet_model.layers:
+    layer.trainable=False
+    
+input_layer = Input(shape=(seq_len, 224, 224, 3))
+
+x = TimeDistributed(resnet_model)(input_layer)
+x = TimeDistributed(Conv2D(kernel_size=1, filters=64))(x) #reduce channels
+
+out1, out2 = Lmu_stack(x, return_sequences=True)
+
+v = velocity_layer(out1)
+
+heat = deconvolution_layer(out2)
+
+model = Model(inputs=input_layer, outputs=[heat,v])
+
+model.summary()
+
+#Data
+X = np.load('X.npy')
+Y = np.load('Y.npy')
+V = np.load('V.npy')
+Y = np.expand_dims(Y, (-1))
+print(X.shape, Y.shape, V.shape)
+
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.fit(X, [Y,V], epochs=30, batch_size=16)
